2e6e4689ad9635aae50b8c5632abaf0968bb31be
AscendedWolf50 <builesjuanjo10@gmail.com>
2025-09-29 22:26:55 -0500

    Arreglo de bug en el panel de victoria, cambios en la textura del personaje principal, implementacion de sonido de checkpoint y cambio en la velocidad de animacion de slide

--- NEON_SHIFT/Assets/Scripts/Checkpoint.cs
+++ NEON_SHIFT/Assets/Scripts/Checkpoint.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+
+    [Header("Audio")]
+    public AudioClip checkpointSound; // sonido al activar checkpoint
+    public float volume = 1f;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -9,12 +14,21 @@ public class Checkpoint : MonoBehaviour
             GameState.Instance.lastCheckpoint = transform.position;
             GameState.Instance.checkpointReached = true;
 
-            // ? Guardar cantidad de monedas actual como la del checkpoint
+            //  Guardar cantidad de monedas actual como la del checkpoint
             if (PlayerCoins.Instance != null)
                 PlayerCoins.Instance.SaveCheckpointCoins();
 
             Debug.Log(" Checkpoint alcanzado en " + transform.position +
                       " | Monedas guardadas: " + GameState.Instance.coins);
+
+            // ?? Reproducir sonido
+            if (checkpointSound != null)
+            {
+                AudioSource playerAudio = other.GetComponentInParent<AudioSource>();
+                if (playerAudio != null)
+                    playerAudio.PlayOneShot(checkpointSound, volume);
+            }
+               
         }
     }
 }
