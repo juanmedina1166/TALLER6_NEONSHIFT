86612691163dc54f4b94d97113d18d6790d8d4a4
AscendedWolf50 <builesjuanjo10@gmail.com>
2025-10-10 13:16:00 -0500

    Implementacion de texto de checkpoint alcanzado

--- NEON_SHIFT/Assets/Scripts/CheckPointUI.cs
+++ NEON_SHIFT/Assets/Scripts/CheckPointUI.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using TMPro;
 
 public class CheckpointUI : MonoBehaviour
 {
-    public static CheckpointUI Instance; // Singleton simple
+    public static CheckpointUI Instance; 
 
     [Header("UI Elements")]
     [SerializeField] private Image checkpointIcon;
+    [SerializeField] private TextMeshProUGUI checkpointText;
 
     private void Awake()
     {
@@ -18,14 +20,18 @@ public class CheckpointUI : MonoBehaviour
 
     public void ShowCheckpointIcon(float duration = 2f)
     {
-        if (checkpointIcon == null) return;
+        if (checkpointIcon && checkpointText == null) return;
         StartCoroutine(ShowIconRoutine(duration));
+        
     }
 
     private IEnumerator ShowIconRoutine(float time)
     {
         checkpointIcon.gameObject.SetActive(true);
+        checkpointText.gameObject.SetActive(true);
         yield return new WaitForSeconds(time);
         checkpointIcon.gameObject.SetActive(false);
+        checkpointText.gameObject.SetActive(false);
+
     }
 }
